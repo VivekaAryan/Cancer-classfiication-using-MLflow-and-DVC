80226644da8d7f1c36337b4b40765d22d9b352e8
Vivek Aryan <aryanravula@gmail.com>
2024-03-15 11:53:07 -0500

    Preapare Base Model added.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from src.ChestCancerClassfication.utils import logger
 from src.ChestCancerClassfication.pipeline.step_01_data_ingestion import DataIngestionTrainingPipeline
+from src.ChestCancerClassfication.pipeline.step_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+
+##################################### Stage 1: Data Ingestion #####################################
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -8,6 +11,21 @@ try:
     obj = DataIngestionTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx=============x")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+##################################### Stage 1: Preparing Base Model #####################################
+
+STAGE_NAME = "Prepare base model"
+
+try:
+    logger.info(f"****************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx=========x")
 
 except Exception as e:
     logger.exception(e)
---
+++ src/ChestCancerClassfication/components/prepare_base_model.py
@@ -0,0 +1,101 @@
+import os
+import urllib.request as request
+from zipfile import ZipFile
+import tensorflow as tf
+from pathlib import Path
+from src.ChestCancerClassfication.entity.config_entity import PrepareBaseModelConfig
+
+
+class PrepareBaseModel:
+    def __init__(self, config: PrepareBaseModelConfig):
+        """
+        Initialize the PrepareBaseModel class with the provided configuration.
+
+        Args:
+            config (PrepareBaseModelConfig): An instance of PrepareBaseModelConfig containing configuration parameters.
+        """
+
+        self.config = config
+
+    def get_base_model(self):
+        """
+        Initialize the base model according to the configuration parameters and save it.
+        """
+        self.model = tf.keras.applications.vgg16.VGG16(
+            input_shape = self.config.params_image_size,
+            weights = self.config.params_weights,
+            include_top = self.config.params_include_top
+        )
+
+        self.save_model(path = self.config.base_model_path, model = self.model)
+        
+    @staticmethod
+    def _prepare_full_model(model, classes, freeze_all, freeze_till, learning_rate):
+        """
+        Prepare and compile the full model based on the provided base model and configuration parameters.
+
+        Args:
+            model (tf.keras.Model): The base model.
+            classes (int): Number of output classes.
+            freeze_all (bool): Whether to freeze all layers of the base model.
+            freeze_till (int or None): Number of layers to freeze from the end of the base model.
+                If None, no layers are frozen.
+            learning_rate (float): Learning rate for the optimizer.
+
+        Returns:
+            tf.keras.Model: The compiled full model.
+        """
+
+        if freeze_all:
+            for layer in model.layers:
+                model.trainable = False
+        
+        elif (freeze_till is not None) and (freeze_till > 0):
+            for layer in model.layers[:-freeze_till]:
+                model.trainable = False
+
+        flatten_in = tf.keras.layers.Flatten()(model.output)
+        prediction = tf.keras.layers.Dense(
+            units = classes,
+            activation = "softmax"
+        )(flatten_in)
+
+        full_model = tf.keras.models.Model(
+            inputs = model.input,
+            outputs = prediction
+        )
+
+        full_model.compile(
+            optimizer = tf.keras.optimizers.SGD(learning_rate = learning_rate),
+            loss = tf.keras.losses.CategoricalCrossentropy(),
+            metrics = ["accuracy"]
+        )
+
+        full_model.summary()
+        return full_model
+
+    def update_base_model(self):
+        """
+        Update the base model by preparing a new full model based on the current base model and configuration parameters,
+        and save the updated model.
+        """
+        self.full_model = self._prepare_full_model(
+            model = self.model,
+            classes = self.config.params_classes,
+            freeze_all = True,
+            freeze_till = None,
+            learning_rate = self.config.params_learning_rate
+        )
+
+        self.save_model(path = self.config.updated_base_model_path, model = self.full_model)
+
+    @staticmethod
+    def save_model(path: Path, model: tf.keras.Model):
+        """
+        Save the provided model to the specified path.
+
+        Args:
+            path (Path): Path where the model will be saved.
+            model (tf.keras.Model): The model to be saved.
+        """
+        model.save(path)
